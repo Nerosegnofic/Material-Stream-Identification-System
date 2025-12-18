992d90dd97786e8f1c82887b8055041aa0540fb8
Ahmed Abdelnabi <77232129+Nerosegnofic@users.noreply.github.com>
2025-12-18 18:00:25 +0200

    Update camera_app.py

--- Material Stream Identification System/realtime/camera_app.py
+++ Material Stream Identification System/realtime/camera_app.py
@@ -2,6 +2,7 @@ import cv2
 import joblib
 import torch
 from PIL import Image
+from sklearn.preprocessing import normalize
 from data_preparation.build_features import CNNFeatureExtractor, base_transform
 from models.rejection import reject_unknown
 from utils.constants import CLASS_NAMES
@@ -10,12 +11,9 @@ MODEL_PATH = "../training/saved_models/svm.pkl"
 THRESHOLD = 0.5
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
 model = joblib.load(MODEL_PATH)
-
 cnn_model = CNNFeatureExtractor().to(device)
 cnn_model.eval()
-
 cap = cv2.VideoCapture(0)
 
 while True:
@@ -24,18 +22,17 @@ while True:
         break
 
     img = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-
     t = base_transform(img).unsqueeze(0).to(device)
 
     with torch.no_grad():
         feat = cnn_model(t).cpu().numpy().reshape(1, -1)
+        feat = normalize(feat, norm='l2')
 
     probs = model.predict_proba(feat)[0]
     label_id = reject_unknown(probs, threshold=THRESHOLD)
     label = "Unknown" if label_id == 6 else CLASS_NAMES[label_id]
 
-    cv2.putText(frame, label, (20, 40),
-                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    cv2.putText(frame, label, (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
     cv2.imshow("Material Classification", frame)
 
     if cv2.waitKey(1) & 0xFF == 27:
